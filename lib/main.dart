ac966ae944ad42f08b7a2f555d1daee99922da15
unknown <mansijain2225@gmail.com>
2021-09-15 22:27:41 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+      home: Text(
+          "Hello world "))); //myapp was flutter team app instead blank material app will confirm to material design (a gogle made designs)
+}
+//home is where everyting starts
+//added a text widget
